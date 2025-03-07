d027f3a96a2f963a6d912a6c39551ac44df87141
LeeKyoWon <kskw20@naver.com>
2025-03-07 17:36:13 +0900

    [Fix][BE] ServiceException 인자 2개인 생성자도 추가

--- backend/src/main/java/com/ll/hotel/global/exceptions/ServiceException.kt
+++ backend/src/main/java/com/ll/hotel/global/exceptions/ServiceException.kt
@@ -2,8 +2,17 @@ package com.ll.hotel.global.exceptions
 
 import org.springframework.http.HttpStatus
 
-class ServiceException(
-    val resultCode: HttpStatus,
-    val msg: String,
-    cause: Throwable? = null
-) : RuntimeException("$resultCode : $msg", cause)
+class ServiceException : RuntimeException {
+    val resultCode: HttpStatus
+    val msg: String
+
+    constructor(resultCode: HttpStatus, msg: String) : super("$resultCode : $msg") {
+        this.resultCode = resultCode
+        this.msg = msg
+    }
+
+    constructor(resultCode: HttpStatus, msg: String, cause: Throwable) : super("$resultCode : $msg", cause) {
+        this.resultCode = resultCode
+        this.msg = msg
+    }
+}
