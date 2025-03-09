6a65eff16ed1c772b46f9b13224b9f610f64460d
universechoi <skye.of.universe@gmail.com>
2025-03-09 14:20:24 +0900

    [be][refactor] @Autowired 생략하여 생성자 주입 방식 개선

--- backend/src/test/java/com/ll/hotel/domain/hotel/option/controller/HotelOptionControllerTest.kt
+++ backend/src/test/java/com/ll/hotel/domain/hotel/option/controller/HotelOptionControllerTest.kt
@@ -7,7 +7,6 @@ import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
@@ -25,7 +24,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 @AutoConfigureMockMvc
 @Transactional
 @WithMockUser(username = "admin", roles = ["ADMIN"])
-class HotelOptionControllerTest @Autowired constructor(
+class HotelOptionControllerTest (
     private val mockMvc: MockMvc,
     private val hotelOptionRepository: HotelOptionRepository
 ) {
