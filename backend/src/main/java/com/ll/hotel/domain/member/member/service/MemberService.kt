dbd128458d84c2fa99b9b1fb44e250c467ee273e
jurio5 <sia5492@naver.com>
2025-03-10 12:08:54 +0900

    [refactor][BE] 다른 클래스 코틀린 마이그레이션 대응

--- backend/src/main/java/com/ll/hotel/domain/member/member/service/MemberService.kt
+++ backend/src/main/java/com/ll/hotel/domain/member/member/service/MemberService.kt
@@ -210,7 +210,7 @@ class MemberService(
         }
 
         actor.favoriteHotels.add(hotel)
-        hotel.getFavorites().add(actor)
+        hotel.favorites.add(actor)
         
         memberRepository.save(actor)
         hotelRepository.save(hotel)
@@ -229,7 +229,7 @@ class MemberService(
         }
 
         actor.favoriteHotels.remove(hotel)
-        hotel.getFavorites().remove(actor)
+        hotel.favorites.remove(actor)
         
         memberRepository.save(actor)
         hotelRepository.save(hotel)
@@ -300,9 +300,9 @@ class MemberService(
         val roleData = HashMap<String, Any>()
         roleData["role"] = member.getUserRole()
         
-        if (member.getUserRole() == "BUSINESS" && member.business?.getHotel() != null) {
+        if (member.getUserRole() == "BUSINESS" && member.business?.hotel != null) {
             roleData["hasHotel"] = true
-            roleData["hotelId"] = member.business!!.getHotel()!!.id
+            roleData["hotelId"] = member.business!!.hotel!!.id
         }
         
         val encodedRoleData = URLEncoder.encode(
