87a873d919923ed794a4a60ff2ffff9e67485b3c
universechoi <skye.of.universe@gmail.com>
2025-03-10 17:11:34 +0900

    [be][chore] MockK 초기화 및 설정 수정으로 테스트 오류 수정

--- backend/src/test/java/com/ll/hotel/domain/member/member/service/BusinessServiceTest.kt
+++ backend/src/test/java/com/ll/hotel/domain/member/member/service/BusinessServiceTest.kt
@@ -3,12 +3,15 @@ package com.ll.hotel.domain.member.member.service
 import com.ll.hotel.domain.member.member.dto.request.BusinessRequest
 import com.ll.hotel.domain.member.member.entity.Member
 import com.ll.hotel.domain.member.member.entity.Role
+import com.ll.hotel.domain.member.member.repository.BusinessRepository
 import com.ll.hotel.domain.member.member.repository.MemberRepository
 import com.ll.hotel.domain.member.member.type.BusinessApprovalStatus
 import com.ll.hotel.domain.member.member.type.MemberStatus
 import com.ll.hotel.global.exceptions.ErrorCode
 import com.ll.hotel.global.exceptions.ServiceException
 import io.mockk.*
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
@@ -24,15 +27,22 @@ import java.time.LocalDate
 @ActiveProfiles("test")
 @Transactional
 class BusinessServiceTest @Autowired constructor(
-    private val businessService: BusinessService,
     private val memberRepository: MemberRepository,
+    private val businessRepository: BusinessRepository
 ) {
-    private val businessValidationService = mockk<BusinessValidationService>()
     private var testId: Long = 0L
+
+    @MockK
+    private lateinit var businessValidationService: BusinessValidationService
+
+    @InjectMockKs
+    private lateinit var businessService: BusinessService
 
     @BeforeEach
     fun setUp() {
-        clearMocks(businessValidationService)
+        MockKAnnotations.init(this, relaxUnitFun = true)
+        clearAllMocks()
+
         testId = memberRepository.save(
             Member(
                 birthDate = LocalDate.now(),
@@ -87,7 +97,7 @@ class BusinessServiceTest @Autowired constructor(
         }
 
         // Then
-        assertThat(exception.resultCode).isEqualTo(ErrorCode.INVALID_BUSINESS_INFO)
+        assertThat(exception.resultCode).isEqualTo(ErrorCode.INVALID_BUSINESS_INFO.httpStatus)
 
         verify { businessValidationService.validateBusiness(businessRequest) }
     }
