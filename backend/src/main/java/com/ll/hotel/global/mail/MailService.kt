fd8413b9b75892f798a38b59e964dcfba948a374
HJ LEE <sete3683@gmail.com>
2025-03-11 16:36:45 +0900

    [STYLE][BE] 코드 스타일 수정

--- backend/src/main/java/com/ll/hotel/global/mail/MailService.kt
+++ backend/src/main/java/com/ll/hotel/global/mail/MailService.kt
@@ -1,7 +1,6 @@
 package com.ll.hotel.global.mail
 
 import com.ll.hotel.domain.booking.booking.dto.BookingResponseDetails
-import com.ll.hotel.domain.member.member.dto.MemberDTO
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
@@ -16,10 +15,9 @@ class MailService {
 
     @Async
     fun sendBookingConfirmedMail(booking: BookingResponseDetails) {
-        val member: MemberDTO = booking.member
-        val subject = "[서울호텔] ${member.memberName} 님의 예약이 확정되었습니다."
+        val subject = "[서울호텔] ${booking.member.memberName} 님의 예약이 확정되었습니다."
         val content = """
-            ${member.memberName} 님의 예약 내역입니다.
+            ${booking.member.memberName} 님의 예약 내역입니다.
 
             예약 번호: ${booking.bookNumber}
             호텔명: ${booking.hotel.hotelName}
@@ -30,15 +28,14 @@ class MailService {
             서울호텔을 이용해주셔서 감사합니다.
         """.trimIndent()
 
-        sendSimpleMail(member.memberEmail, subject, content)
+        sendSimpleMail(booking.member.memberEmail, subject, content)
     }
 
     @Async
     fun sendBookingCancelledMail(booking: BookingResponseDetails) {
-        val member: MemberDTO = booking.member
-        val subject = "[서울호텔] ${member.memberName} 님의 예약이 취소되었습니다."
+        val subject = "[서울호텔] ${booking.member.memberName} 님의 예약이 취소되었습니다."
         val content = """
-            ${member.memberName} 님의 예약이 ${booking.modifiedAt.format(DateTimeFormatter.ofPattern("MM월 dd일 HH시 mm분"))}에 취소되었습니다.
+            ${booking.member.memberName} 님의 예약이 ${booking.modifiedAt.format(DateTimeFormatter.ofPattern("MM월 dd일 HH시 mm분"))}에 취소되었습니다.
 
             예약 번호: ${booking.bookNumber}
             호텔명: ${booking.hotel.hotelName}
@@ -49,7 +46,7 @@ class MailService {
             서울호텔을 이용해주셔서 감사합니다.
         """.trimIndent()
 
-        sendSimpleMail(member.memberEmail, subject, content)
+        sendSimpleMail(booking.member.memberEmail, subject, content)
     }
 
     private fun sendSimpleMail(to: String, subject: String, content: String) {
